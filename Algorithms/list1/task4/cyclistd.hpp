df1e9f6cfca9bf8a8b1a8f7f737725b3287ad609
ShrikeBin <grahamka.ha@gmail.com>
2025-03-09 12:02:16 +0100

    idk

---
+++ Algorithms/list1/task4/cyclistd.hpp
@@ -0,0 +1,102 @@
+#ifndef CYCLISTD_HPP
+#define CYCLISTD_HPP
+
+#include <iostream>
+
+template <typename T>
+struct Node 
+{
+    T data;
+    Node* next;
+    Node* prev;
+};
+
+template <typename T>
+class CycListD 
+{
+private:
+    Node<T>* front;
+    unsigned long elementcount;
+
+public:
+    CycListD() : front(nullptr), elementcount(0) {}
+
+    void insert(T value) 
+    {
+        Node<T>* newNode = new Node<T>{value, nullptr, nullptr};
+        
+        if (front == nullptr) 
+        {
+            front = newNode;
+            front->next = front;
+            front->prev = front;
+        } 
+        else 
+        {
+            newNode->next = front->next;
+            newNode->prev = front;
+            front->next->prev = newNode;
+            front->next = newNode;
+            front = newNode;
+        }
+
+        elementcount++;
+    }
+
+    void merge(CycListD<T>& list) 
+    {
+        if (list.isEmpty()) return;
+        if (isEmpty()) 
+        {
+            front = list.front;
+            elementcount = list.elementcount;
+            list.front = nullptr;
+            list.elementcount = 0;
+            return;
+        }
+
+        Node<T>* temp1 = front->next;
+        Node<T>* temp2 = list.front->next;
+        
+        front->next = temp2;
+        temp2->prev = front;
+        
+        list.front->next = temp1;
+        temp1->prev = list.front;
+        
+        front = list.front;
+
+        elementcount += list.elementcount;
+        list.front = nullptr;
+        list.elementcount = 0;
+    }
+
+    bool isEmpty() const 
+    {
+        return front == nullptr;
+    }
+
+    void clean() 
+    {
+        if (isEmpty()) return;
+
+        Node<T>* current = front->next;
+        while (current != front) 
+        {
+            Node<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
+
+        delete front;
+        front = nullptr;
+        elementcount = 0;
+    }
+
+    ~CycListD() 
+    {
+        clean();
+    }
+};
+
+#endif
