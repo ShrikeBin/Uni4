ad086200f6fe6efaa4885138002c9a2a679c4596
ShrikeBin <grahamka.ha@gmail.com>
2025-03-11 11:46:38 +0100

    some changes

---
+++ Funcional/FirstSteps/main.hs
@@ -0,0 +1,20 @@
+-- Silnia : IF ... ELSE ...
+fact1 :: Integer -> Integer
+fact1 n = if n==0 then 1
+          else n * fact1 (n-1)
+
+-- Silnia : pattern matching          
+fact2 :: Integer -> Integer
+fact2 0 = 1
+fact2 n = n * fact2 (n - 1)
+
+-- Silnia : case expresion
+fact3 :: Integer -> Integer
+fact3 n = case n of 
+  0 -> 1
+  n -> n * fact3 (n-1)
+
+-- Silnia : with let expression  
+fact4 :: Integer -> Integer
+fact4 n  = let y = fact4 (n-1) in
+           if n==0 then 1 else n * y
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Uni sem4
+- Wstęp do Sztucznej Intelignecji
+- Algorytmy i Struktury Danych
+- Programowanie Funkcyjne
+- Programowanie współbieżne
+- Technologie Sieciowe
