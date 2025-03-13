50604d2b8af024694a43ae5e1d140c6adcba0965
ShrikeBin <grahamka.ha@gmail.com>
2025-03-13 11:41:50 +0100

    rest later

--- Parallel/list0/ada/main.adb
+++ Parallel/list0/ada/main.adb
@@ -4,17 +4,17 @@ use Ada.Text_IO;
 use Ada.Integer_Text_IO;
 
 procedure Main is
-    n : Integer;
-    result : Integer;
+   n : Integer;
+   result : Integer;
 
-    function Factorial (n : Integer) return Integer is
-    begin
-        if n = 0 then
-            return 1;
-        else
-            return n * Factorial (n - 1);
-        end if;
-    end Factorial;
+   function Factorial (n : Integer) return Integer is
+   begin
+      if n = 0 then
+         return 1;
+      else
+         return n * Factorial (n - 1);
+      end if;
+   end Factorial;
 
 begin
    Put("Enter n: ");
